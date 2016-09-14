2192ce01aa8b0c87a04ef7238b4345e093020b8d
rafaelskubisz <jenovas00@gmail.com>
2016-09-14 23:17:59 +0200

    Remove no longer needed files

--- PULPulsate/PULPulsateBasic.bundle/NotificationViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/rafaelskubisz/XcodeProjects/Pulsate-iOS-SDK-Source-Refactored/PULPulsate/PulsateNotificationTest/NotificationViewController.m \
-  /Users/rafaelskubisz/XcodeProjects/Pulsate-iOS-SDK-Source-Refactored/PULPulsate/PulsateNotificationTest/NotificationViewController.h

b080c8793a7a503b4a3ef0c9cdb7894efff848b0
Rafael Skubisz <rafael.skubisz@pulsatehq.com>
2017-03-22 12:40:04 +0100

    Release 2.9.15: 1. When the user is in the inbox push notification will no longer show up. Instead the user’s inbox will refresh and a “New Message” pill will show up. 2. Fixed a bug where pushes would not take you to the proper destination if the app was killed. 3. New method - disablePushNotification - Disables or Enables push notifications for Pulsate

--- PULPulsate/PULPulsate.framework/Headers/PULPulsateManager.h
+++ PULPulsate/PULPulsate.framework/Headers/PULPulsateManager.h
@@ -1,6 +1,6 @@
 //
 //  PULPulsateManager.h
-//  PULPulsate 2.9.12
+//  PULPulsate 2.9.15
 //
 //  Created by Michal on 04/12/2014.
 //  Copyright (c) 2014 Pulsatehq. All rights reserved.
@@ -76,6 +76,13 @@ typedef NS_ENUM(NSUInteger, PULPrivacyLevel){
  *  @param gender user's gender
  */
 -(void)updateGender:(PULUserGender)gender;
+
+/**
+ *  Disables or Enables push notifications for Pulsate
+ *
+ *  @param disable user's push notification preference
+ */
+-(void)disablePushNotification:(BOOL)disable;
 
 /**
  *  Sets user's privacy settings.

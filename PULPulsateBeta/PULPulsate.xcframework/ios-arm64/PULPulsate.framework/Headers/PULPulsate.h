06e8a6400c77cd67b6b34eae7509de36f2aa2dfa
Ali N <ali@pulsatehq.com>
2025-03-04 15:03:12 +0500

    framework updated

---
+++ PULPulsateBeta/PULPulsate.xcframework/ios-arm64/PULPulsate.framework/Headers/PULPulsate.h
@@ -0,0 +1,13 @@
+//
+//  PULPulsate.h
+//  PULPulsate
+//
+//  Created by Michal on 04/12/2014.
+//  Copyright (c) 2014 Pulsatehq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PULSystemKeychain.h"
+#import "BSBacktraceLogger.h"
+#import "FBShimmeringView.h"
